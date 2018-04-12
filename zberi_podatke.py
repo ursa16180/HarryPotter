fa8bf2fcd9d573f9a3c7c7649e930432b560743f
Nina Stempelj <nina.stempelj@gmail.com>
2018-04-12 11:18:36 +0200

    zajem podatkov

--- zberi_podatke.py
+++ zberi_podatke.py
@@ -2,21 +2,25 @@ import requests
 import re
 import orodja
 
-vzorec_linka = re.compile(r'<strong\s*?class=\Wuitext\s*?result\W>\s*?(?P<glasov>\d?\d?\d?,?\d?\d?\d)\s*?votes\s*?'
-                          r'</strong>\s*?<div\s*?class=\WanswerWrapper\W>\s*?<div\s*?class=\Wjs-tooltip'
-                          r'Trigger\s*?tooltipTrigger\W\s*?data-placement=\W\w+?\W\s*?data-resource-id=\W\d+'
-                          r'\W\s*?data-resource-type=\WGCA\W>\s*?<a\s*?id="bookCover_\d+?"\s*?class="'
-                          r'pollAnswer__bookLink"\s*?href="(?P<url>.*?)"><img\s*?alt=".*?"\s*?title=')
-orodja.shrani_stran('https://www.goodreads.com/list/show/559.What_To_Read_After_Harry_Potter?page=1', 'testna.txt', vsili_prenos=False)
-##
-##for stran in range(1, 15):
-##    r = requests.get('https://www.goodreads.com/list/show/559.What_To_Read_After_Harry_Potter?page={}'.format(stran))
-##    page_source = r.text
-##    linki = []
-##    for zadetek in re.finditer(vzorec_linka, stran):
-##        linki += [('https://www.goodreads.com' + zadetek.groupdict()['url'], zadetek.groupdict()['glasov'])]
-##    print(len(linki))
-##    for par in linki:
-##        link = par[0]
-##        glasov = par[1]
-##        orodja.shrani_stran(link, 'podatki/{}-{}-{}.html'.format(leto, zanr, glasov))
+vzorec_linka = re.compile(r"""<td width="100%" valign="top">\s*?
+                          <a class="bookTitle" itemprop="url" href="(?P<link_knjige>.*?)">\s*?
+                          <span itemprop='name'>(?P<naslov>.*?)</span>\s*?</a>\s*?<br/>\s*?
+                          <span class='by smallText'>by</span>\s*?
+                          <span itemprop='author' itemscope='' itemtype='http://schema.org/Person'>\s*?
+                          <a class="authorName" itemprop="url" href=".*?"><span itemprop="name">
+                          .*?</span></a>""")
+
+vzorec_linka2 = re.compile(r"<td width=\"100%\" valign=\"top\">\s*?<a class=\"bookTitle\" itemprop=\"url\" href=\"(?P<link_knjige>.*?)\">\s*?<span itemprop='name'>(?P<naslov>.*?)</span>\s*?</a>\s*?<br/>\s*?<span class='by smallText'>by</span>\s*?<span itemprop='author' itemscope='' itemtype='http://schema.org/Person'>\s*?<a class=\"authorName\" itemprop=\"url\" href=\".*?\"><span itemprop=\"name\">.*?</span></a>")
+# orodja.shrani_stran('https://www.goodreads.com/list/show/559.What_To_Read_After_Harry_Potter?page=1', 'testna.txt', vsili_prenos=False)
+
+for stran in range(1, 2):
+    r = requests.get('https://www.goodreads.com/list/show/559.What_To_Read_After_Harry_Potter?page={}'.format(stran))
+    page_source = r.text
+    linki = []
+    for zadetek in re.finditer(vzorec_linka2, page_source):
+        print('ah ja')
+        print(zadetek.groupdict())
+        linki += [('https://www.goodreads.com' + zadetek.groupdict()['link_knjige'], zadetek.groupdict()['naslov'])]
+    print(len(linki))
+    for link in linki:
+        orodja.shrani_stran(link[0], 'knjige/{}.html'.format(link[1]))
