c35c670304e5d1852e78f1e27c3c14efac66e66d
Urša Pertot <ursa.pertot@student.fmf.uni-lj.si>
2018-04-26 13:59:37 +0200

    Datoteke za delanje tabel

--- avtor_knjige.py
+++ avtor_knjige.py
@@ -3,46 +3,46 @@ import auth
 
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)  # se znebimo problemov s šumniki
 
 import csv
 
+
 def ustvari_tabelo():
     cur.execute("""
-        CREATE TABLE knjiga (
-            ISBN TEXT PRIMARY KEY,
-            naslov TEXT NOT NULL,
-            ocena FLOAT,
-            stevilo_ocen INTEGER,
-            leto_izdaje INTEGER, 
-            dolzina INTEGER NOT NULL,
-            povzetek TEXT NOT NULL
-            
+        CREATE TABLE avtor_knjige (
+            id_avtorja TEXT NOT NULL REFERENCES avtor(id),
+            ISBN_knjige TEXT NOT NULL REFERENCES knjiga(ISBN),
+            PRIMARY KEY (id_avtorja, ISBN_knjige)
         );
     """)
     ### zanr TEXT NOT NULL FOREIGN KEY, zbirka TEXT FOREIGN KEY, kljucne_besede TEXT FOREIGN KEY, avtor TEXT NOT NULL FOREIGN KEY,
+    print("Narejena tabela AVTOR_KNJIGE")
     conn.commit()
+
 
 def pobrisi_tabelo():
     cur.execute("""
-        DROP TABLE knjiga;
+        DROP TABLE avtor_knjige;
     """)
+    print("Izbrisna tabela AVTOR_KNJIGE")
     conn.commit()
 
+
 def uvozi_podatke():
-    with open("csv/knjiga_poskus.csv") as f:
+    with open("csv/avtor_knjige_poskus.csv") as f:
         rd = csv.reader(f)
-        next(rd) # izpusti naslovno vrstico ###TODO?
+        next(rd)  # izpusti naslovno vrstico ###TODO?
         for r in rd:
             r = [None if x in ('', '-') else x for x in r]
             cur.execute("""
-                INSERT INTO knjiga
-                (ISBN, naslov, ocena, stevilo_ocen, leto_izdaje, dolzina, povzetek)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                RETURNING ISBN
+                INSERT INTO avtor_knjige
+                (id_avtorja, ISBN_knjige)
+                VALUES (%s, %s)
             """, r)
             rid, = cur.fetchone()
-            print("Uvožena knjiga %s s ISBN-jem %s" % (r[1], rid))
+            print("Uvožen knjiga z ISBNjem %s avtorja z IDjem %s" % (r[1], r[0]))
     conn.commit()
 
 
@@ -50,3 +50,4 @@ conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, passwo
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 #ustvari_tabelo()
 #uvozi_podatke()
+#pobrisi_tabelo()
--- serija.py
+++ serija.py
@@ -3,46 +3,46 @@ import auth
 
 # uvozimo psycopg2
 import psycopg2, psycopg2.extensions, psycopg2.extras
-psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)  # se znebimo problemov s šumniki
 
 import csv
 
+
 def ustvari_tabelo():
     cur.execute("""
-        CREATE TABLE knjiga (
-            ISBN TEXT PRIMARY KEY,
-            naslov TEXT NOT NULL,
-            ocena FLOAT,
-            stevilo_ocen INTEGER,
-            leto_izdaje INTEGER, 
-            dolzina INTEGER NOT NULL,
-            povzetek TEXT NOT NULL
-            
+        CREATE TABLE serija (
+            id TEXT PRIMARY KEY,
+            ime TEXT NOT NULL
         );
     """)
     ### zanr TEXT NOT NULL FOREIGN KEY, zbirka TEXT FOREIGN KEY, kljucne_besede TEXT FOREIGN KEY, avtor TEXT NOT NULL FOREIGN KEY,
+    print("Narejena tabela SERIJA")
     conn.commit()
+
 
 def pobrisi_tabelo():
     cur.execute("""
-        DROP TABLE knjiga;
+        DROP TABLE serija;
     """)
+    print("Izbrisna tabela SERIJA")
     conn.commit()
 
+
 def uvozi_podatke():
-    with open("csv/knjiga_poskus.csv") as f:
+    with open("csv/serija_poskus.csv") as f:
         rd = csv.reader(f)
-        next(rd) # izpusti naslovno vrstico ###TODO?
+        next(rd)  # izpusti naslovno vrstico ###TODO?
         for r in rd:
             r = [None if x in ('', '-') else x for x in r]
             cur.execute("""
-                INSERT INTO knjiga
-                (ISBN, naslov, ocena, stevilo_ocen, leto_izdaje, dolzina, povzetek)
-                VALUES (%s, %s, %s, %s, %s, %s, %s)
-                RETURNING ISBN
+                INSERT INTO serija
+                (id, ime)
+                VALUES (%s, %s)
+                RETURNING id
             """, r)
             rid, = cur.fetchone()
-            print("Uvožena knjiga %s s ISBN-jem %s" % (r[1], rid))
+            print("Uvožena serija %s z id-jem %s" % (r[1], rid))
     conn.commit()
 
 
@@ -50,3 +50,4 @@ conn = psycopg2.connect(database=auth.db, host=auth.host, user=auth.user, passwo
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 #ustvari_tabelo()
 #uvozi_podatke()
+#pobrisi_tabelo()
