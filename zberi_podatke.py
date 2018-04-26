b75fdff8ddd472ccd4de817765ac0cfb36806c96
Nina Stempelj <nina.stempelj@gmail.com>
2018-04-26 13:31:58 +0200

    Popravljena imena datotek, da delajo tudi dvopičja.

--- zberi_podatke.py
+++ zberi_podatke.py
@@ -2,29 +2,25 @@ import requests
 import re
 import orodja
 
-vzorec_linka = re.compile(r"""<td width="100%" valign="top">\s*?
-                          <a class="bookTitle" itemprop="url" href="(?P<link_knjige>.*?)">\s*?
-                          <span itemprop='name'>(?P<naslov>.*?)</span>\s*?</a>\s*?<br/>\s*?
-                          <span class='by smallText'>by</span>\s*?
-                          <span itemprop='author' itemscope='' itemtype='http://schema.org/Person'>\s*?
-                          <a class="authorName" itemprop="url" href=".*?"><span itemprop="name">
-                          .*?</span></a>""")
+vzorec_linka = re.compile("""<td width="100%" valign="top">\s*?<a class="bookTitle" itemprop="url" href="(?P<link_knjige>.*?)">\s*?<span itemprop='name'>(?P<naslov>.*?)</span>\s*?</a>\s*?<br/>\s*?<span class='by smallText'>by</span>""")
 
-vzorec_linka2 = re.compile(r"<td width=\"100%\" valign=\"top\">\s*?<a class=\"bookTitle\" itemprop=\"url\" href=\"(?P<link_knjige>.*?)\">\s*?<span itemprop='name'>(?P<naslov>.*?)</span>\s*?</a>\s*?<br/>\s*?<span class='by smallText'>by</span>\s*?<span itemprop='author' itemscope='' itemtype='http://schema.org/Person'>\s*?<a class=\"authorName\" itemprop=\"url\" href=\".*?\"><span itemprop=\"name\">.*?</span></a>")
-# orodja.shrani_stran('https://www.goodreads.com/list/show/559.What_To_Read_After_Harry_Potter?page=1', 'testna.txt', vsili_prenos=False)
+#vzorec_linka2 = re.compile(r"<td width=\"100%\" valign=\"top\">\s*?<a class=\"bookTitle\" itemprop=\"url\" href=\"(?P<link_knjige>.*?)\">\s*?<span itemprop='name'>(?P<naslov>.*?)</span>\s*?</a>\s*?<br/>\s*?<span class='by smallText'>by</span>\s*?<span itemprop='author' itemscope='' itemtype='http://schema.org/Person'>\s*?<a class=\"authorName\" itemprop=\"url\" href=\".*?\"><span itemprop=\"name\">.*?</span></a>")
+#orodja.shrani_stran('https://www.goodreads.com/list/show/559.What_To_Read_After_Harry_Potter?page=1', 'testna.txt', vsili_prenos=False)
 
-# poberemo vse linke do knjig z vseh 10 srani (zaenkrat samo 1.):
+# poberemo vse linke do knjig z vseh 10 strani (zaenkrat samo 1.):
 for stran in range(1, 2):
     r = requests.get('https://www.goodreads.com/list/show/559.What_To_Read_After_Harry_Potter?page={}'.format(stran))
     page_source = r.text
     linki = []
-    for zadetek in re.finditer(vzorec_linka2, page_source):
-        # print('ah ja')
-        print(zadetek.groupdict())
-        linki += [('https://www.goodreads.com' + zadetek.groupdict()['link_knjige'], zadetek.groupdict()['naslov'])]
-# če je v naslovu dvopičje, pride do napake
+    for zadetek in re.finditer(vzorec_linka, page_source):
+        print('ah ja')
+# če je v naslovu dvopičje, pride do napake, 
         if ':' in zadetek.groupdict()['naslov']:
-            print(zadetek.groupdict()['naslov'])
-    #for link in linki:
-    #    orodja.shrani_stran(link[0], 'knjige/{}.html'.format(link[1]))
+            popravljen_naslov = zadetek.groupdict()['naslov']
+            linki += [('https://www.goodreads.com' + zadetek.groupdict()['link_knjige'], popravljen_naslov.replace(':','-'))]
+        else:
+            linki += [('https://www.goodreads.com' + zadetek.groupdict()['link_knjige'], zadetek.groupdict()['naslov'])]
+    for link in linki:
+            orodja.shrani_stran('https://www.goodreads.com' + link[0],'knjige/{}.html'.format(link[1]))
 
+print(len(linki))
