3b85a375ad573e5bdc611144bdf07f62a777fade
Ursa Pertot <ursa.pertot@gmail.com>
2018-04-28 15:28:39 +0200

    Popravljeno za pojavitev poševnice v naslovu

--- zberi_podatke.py
+++ zberi_podatke.py
@@ -18,9 +18,16 @@ for stran in range(1, 2):
         if ':' in zadetek.groupdict()['naslov']:
             popravljen_naslov = zadetek.groupdict()['naslov']
             linki += [('https://www.goodreads.com' + zadetek.groupdict()['link_knjige'], popravljen_naslov.replace(':','-'))]
+#če je v naslovu slash pride do napake
+        elif '/' in zadetek.groupdict()['naslov']:
+            popravljen_naslov = zadetek.groupdict()['naslov']
+            ###TODO če ima obe napaki, se nama bo naslov dvakrat vnesel v seznam
+            linki += [('https://www.goodreads.com' + zadetek.groupdict()['link_knjige'], popravljen_naslov.replace('/','-'))] ###pazit morva da če ima serija 2 imeni jih ločiva
         else:
             linki += [('https://www.goodreads.com' + zadetek.groupdict()['link_knjige'], zadetek.groupdict()['naslov'])]
     for link in linki:
-            orodja.shrani_stran('https://www.goodreads.com' + link[0],'knjige/{}.html'.format(link[1]))
+        print(link)
+        #orodja.shrani_stran('https://www.goodreads.com' + link[0],'knjige/{}.html'.format(link[1])) ###link že vsebuje www.goodreads...
+        orodja.shrani_stran(link[0], 'knjige/{}.html'.format(link[1]))
 
 print(len(linki))
