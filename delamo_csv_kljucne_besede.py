6f992093d5e3ee21bfac1f045e27507f9a78e69f
Ursa Pertot <ursa.pertot@gmail.com>
2018-05-24 23:46:11 +0200

    Malo dopolnjeno za dodajanje kljucnih besed

--- delamo_csv_kljucne_besede.py
+++ delamo_csv_kljucne_besede.py
@@ -1,10 +1,12 @@
-seznam_besed = ['Pojem', 'Headmaster', 'School', 'Student', 'Castle', 'Boarding school', 'Beast', 'Goblin', 'Centaur',
+import orodja
+
+seznam_besed = ['Headmaster', 'School', 'Student', 'Castle', 'Boarding school', 'Beast', 'Goblin', 'Centaur',
                 'Giant',
                 'Phoenix', 'Witch', 'Wizard', 'Dragon', 'Boggart', 'Vampire', 'Monster', 'Elf', 'Dwarf', 'Demigod',
                 'Alien',
-                'Werewolf', 'Unicorn', 'Rat', 'Owl', 'Sphinx', 'Troll', 'Hobbit', 'Fairy', 'Human', 'Angel', 'Demon',
+                'Werewolf', 'Unicorn', ' Rat ',' Rats ', ' Owl', 'Sphinx', 'Troll', 'Hobbit', 'Fairy', 'Human', 'Angel', 'Demon',
                 'God',
-                'mystical creatures ', 'Prince', 'Princess', 'King', 'Queen', 'Prisoner', 'Orphan', 'Muggle', 'Witch',
+                'mystical creatures ', 'Prince', 'Princess',  'Queen', 'Prisoner', 'Orphan', 'Muggle', 'Witch',
                 'Wizard',
                 'Dark Lord', 'Apprentice', 'Friend', 'Enemy', 'Magic', 'Witchcraft', 'Wizardry', 'Charms',
                 'Transfiguration', 'Curse',
@@ -16,7 +18,7 @@ seznam_besed = ['Pojem', 'Headmaster', 'School', 'Student', 'Castle', 'Boarding 
                 'Pure Blood',
                 'Choice', 'Friendship', 'Imagination', 'eternal life', 'Family', 'Goblet', 'Potion', 'Wand',
                 'Flying broom', 'Train',
-                'Socks', 'Mirror', 'Hat', 'Letter', 'Sword', 'Chess', 'Scar', 'Pig tail', 'Chocolate', 'Parchment',
+                'Socks', 'Mirror', 'Letter', 'Sword', 'Chess', 'Scar', 'Pig tail', 'Chocolate', 'Parchment',
                 'Maze',
                 'Voldemort', 'Hermione Granger', 'Harry Potter', 'Ron Weasley', 'Nicholas Flamel', 'marauders ', 'Duel',
                 'Invisible',
@@ -26,19 +28,42 @@ seznam_besed = ['Pojem', 'Headmaster', 'School', 'Student', 'Castle', 'Boarding 
                 'Tom Riddle',
                 'Leacky cauldron', 'Orphanage', 'Outsider', 'Mission', 'Quest', 'Calling', 'Save the world',
                 'Destroy the base',
-                'Find ']
+                'Find',' King']#,'Hat']
 
 seznam_vseh_knjig_kljucnih_besed = []
 dodane_knjige=set()
+#mankajoce = []
+
+frekvenca=dict()
+for pojem in seznam_besed:
+    frekvenca[pojem.lower()]=0
 
 def poisci_kljucne_besede(seznam_vseh_knjig):
     for knjiga in seznam_vseh_knjig:
         opis = knjiga['opis'].lower()
+        naslov = knjiga['naslov'].lower()
         for beseda in seznam_besed:
-            if beseda.lower() in opis:
+            if beseda.lower() in opis + naslov:
                 kljucna_beseda = dict()
                 kljucna_beseda['id_knjige'] = knjiga['id']
                 kljucna_beseda['kljucna_beseda'] = beseda
                 seznam_vseh_knjig_kljucnih_besed.append(kljucna_beseda)
                 dodane_knjige.add(knjiga['id'])
+                frekvenca[beseda.lower()]+=1
+    #     if knjiga['id'] not in dodane_knjige: #Naredi csv knjig, ki niso imele nobene kljucne besede
+    #         mankajoce.append(knjiga)
+    # orodja.zapisi_tabelo(mankajoce,
+    #                      ['id', 'ISBN', 'naslov', 'dolzina', 'povprecna_ocena', 'stevilo_ocen', 'leto', 'opis'],
+    #                      'podatki/mankajoce.csv')
+
     print(len(dodane_knjige), len(seznam_vseh_knjig_kljucnih_besed))
+    print(sorted(frekvenca,key=frekvenca.get,reverse=True)) #Vrne seznam od najpogostejše do neobstojece
+
+def naredi_seznam_kljucnih_besed():
+    datoteka = open('kljucni.csv', 'r')
+    seznam_kljucnih = []
+    for vrstica in datoteka:
+        (pojem, skupina) = vrstica.split(';')
+        seznam_kljucnih.append(pojem)
+    datoteka.close()
+    return seznam_kljucnih
